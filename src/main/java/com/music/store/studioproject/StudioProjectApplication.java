a7b9f9d17fdfa77d0651bc35771dd044b1ca36f7
Anasiotasio <Zxl2024903531@outlook.com>
2025-09-30 00:45:19 +0800

    更新配置文件，调整日志级别，修改依赖版本以兼容Spring Boot 3.2.x

--- src/main/java/com/music/store/studioproject/StudioProjectApplication.java
+++ src/main/java/com/music/store/studioproject/StudioProjectApplication.java
@@ -1,9 +1,11 @@
 package com.music.store.studioproject;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.music.store.studioproject.dao")
 public class StudioProjectApplication {
 
     public static void main(String[] args) {
